24ee6107b9a44d88585e875f866f1f26a2ecdc8b
HyotonStorm <jonathan.prost@epitech.eu>
2016-11-27 16:42:38 +0100

    v 1.0

--- Data/Agenda.js
+++ Data/Agenda.js
@@ -3,17 +3,53 @@ console.log("Get Type de Données : Agenda.");
 
 function getVodIpTv(){
 
-	var EvenementData;
+	var EvenementData = {
+	_description: [],
+	_start: [],
+	_end: [],
+	_place: []	
+	}
 
-	initAgenda();
+	initAgenda_description();
+	initAgenda_start();
+	initAgenda_end();
+	initAgenda_place();
+
 	return EvenementData;	
 }
 
-function initAgenda(callback) {
+function initAgenda_description(callback) {
 	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.description); }', function(err) {
 		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
 			console.log(docs);
-			EvenementData = docs;
+			EvenementData._description = docs;
+		});
+	});
+};
+
+function initAgenda_start(callback) {
+	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.start); }', function(err) {
+		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
+			console.log(docs);
+			EvenementData.start = docs;
+		});
+	});
+};
+
+function initAgenda_end(callback) {
+	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.end); }', function(err) {
+		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
+			console.log(docs);
+			EvenementData._end = docs;
+		});
+	});
+};
+
+function initAgenda_place(callback) {
+	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.start); }', function(err) {
+		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
+			console.log(docs);
+			EvenementData._place = docs;
 		});
 	});
 };
--- Data/VodIpTv.js
+++ Data/VodIpTv.js
@@ -4,26 +4,23 @@ console.log(" Get Type de Données : VodIpTv.");
 function getVodIpTv(){
 
 	var VodIpTvData = {
-	_doc: [],
+	_title: [],
 	_thematique: []	
 	}
+	// API WikiData.
+
+	// API Cozy.
+	var VoidipTv_title = [];
 	initVodIpTv_title();
-	for (var i = 0; i < VodIpTvData.doc.length; i++)		
-		VoidipTvData._thematique[i] = getGenre(VoidipTvData._doc[i].title)
 	return VoidipTv_title;	
+}
 
 function initVodIpTv_title(callback) {
 	cozysdk.defineView('VideoStream', 'voidiptv', 'function(doc) { emit(doc.title); }', function(err) {
 		cozysdk.queryView('VideoStream', 'voidiptv', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
 			console.log(docs);
-			VoidipTvData._doc = docs;
+			VoidipTv_title = docs;
 		});
 	});
 };
 
-
-fonction getGenre(movie)
-{
-
-
-}
