7f96fc9a902252037dab0db763fd1c0971f95cfb
Fontanet <nicolas.fontanet@epitech.eu>
2017-02-10 17:04:26 +0100

    Update Agenda.js

--- Data/Agenda.js
+++ Data/Agenda.js
@@ -1,14 +1,14 @@
  //Type de Données : Agenda.
 console.log("Get Type de Données : Agenda.");
 
-function getVodIpTv(){
-
-	var EvenementData = {
-	_description: [],
-	_start: [],
-	_end: [],
-	_place: []	
-	}
+function getVodIpTv()
+{
+	var EvenementData = { // Déclaration d'un object
+			_description: [],
+			_start: [],
+			_end: [],
+			_place: []	
+			}
 
 	initAgenda_description();
 	initAgenda_start();
@@ -18,38 +18,78 @@ function getVodIpTv(){
 	return EvenementData;	
 }
 
-function initAgenda_description(callback) {
-	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.description); }', function(err) {
-		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
-			console.log(docs);
-			EvenementData._description = docs;
-		});
-	});
+function initAgenda_description(callback) 
+{
+	cozysdk.defineView('Event',
+			   'monagenda', 
+			   'function(doc) { emit(doc.description); }',
+			   function(err) {
+						cozysdk.queryView('Event', 
+								  'monagenda',
+								  {"limit":50, "include_docs":true, "descending":true}, 
+								  function(err, docs) {
+										console.log(docs);
+										EvenementData._description = docs;
+										      }
+								 );
+					}
+			  );
 };
 
-function initAgenda_start(callback) {
-	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.start); }', function(err) {
-		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
-			console.log(docs);
-			EvenementData.start = docs;
-		});
-	});
+function initAgenda_start(callback)
+{
+	cozysdk.defineView('Event',
+			   'monagenda', 
+			   'function(doc) { emit(doc.start); }', 
+			   function(err) 
+			   {
+				cozysdk.queryView('Event', 
+						  'monagenda',
+						  {"limit":50, "include_docs":true, "descending":true},
+						  function(err, docs) 
+						  {
+							console.log(docs);
+							EvenementData.start = docs;
+						  }
+						);
+			  }
+			  );
 };
 
-function initAgenda_end(callback) {
-	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.end); }', function(err) {
-		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
-			console.log(docs);
-			EvenementData._end = docs;
-		});
-	});
+function initAgenda_end(callback) 
+{
+				cozysdk.defineView('Event',
+						   'monagenda', 
+						   'function(doc) { emit(doc.end); }',
+						   function(err) {
+								cozysdk.queryView('Event', 
+										  'monagenda',
+										  {"limit":50, "include_docs":true, "descending":true},
+										  function(err, docs) 
+										  {
+											console.log(docs);
+											EvenementData._end = docs;
+										  }
+										 );
+								 }
+						  );
 };
 
-function initAgenda_place(callback) {
-	cozysdk.defineView('Event', 'monagenda', 'function(doc) { emit(doc.start); }', function(err) {
-		cozysdk.queryView('Event', 'monagenda', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
-			console.log(docs);
-			EvenementData._place = docs;
-		});
-	});
+function initAgenda_place(callback) 
+{
+				cozysdk.defineView('Event',
+						   'monagenda',
+						   'function(doc) { emit(doc.start); }',
+						   function(err) {
+							cozysdk.queryView('Event',
+									  'monagenda',
+									  {"limit":50, "include_docs":true, "descending":true},
+									  function(err, docs) 
+									  {
+										console.log(docs);
+										EvenementData._place = docs;
+									  }
+									 );
+								}
+						  );
 };
