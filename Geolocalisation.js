dadf532853782211e9296471766c063aad2513d4
Fontanet <nicolas.fontanet@epitech.eu>
2017-02-10 16:57:29 +0100

    Update Geolocalisation.js

--- Geolocalisation.js
+++ Geolocalisation.js
@@ -1,8 +1,7 @@
 // Type de Données : Culturelle.
 console.log("Get Type de Données : Geolocation.");
 
-
-
+// Déclaration de la fonction.
 function getGeoloc()
 {
 	var _geoData;
@@ -13,20 +12,29 @@ function getGeoloc()
 
 // MesInfos Data
 function initGeoloc(callback) {
-	cozysdk.defineView('GeoPoint', 'byTimestamp', 'function(doc) { emit(doc.timestamp); }', function(err) {
-		cozysdk.queryView('GeoPoint', 'byTimestamp', {"limit":50, "include_docs":true, "descending":true}, function(err, docs) {
-			console.log(docs);
-			_geoData = docs;
-		});
-	});
+	  cozysdk.defineView('GeoPoint',
+			     'byTimestamp', 
+			     'function(doc) { emit(doc.timestamp); }',
+			     function(err) 
+			     {
+				cozysdk.queryView('GeoPoint',
+						  'byTimestamp',
+						  {"limit":50, "include_docs":true, "descending":true},
+						  function(err, docs) {
+									console.log(docs);
+									_geoData = docs;
+									}
+						);
+			    }
+			    );
 };
 
 // Geolocalisation navigateur.
 /*fonction initGeoloc_navigator()
 {
-		var _geoData = {
-	_Latitude: 0,
-	_Longitude: 0,
+	var _geoData = {
+		_Latitude: 0,
+		_Longitude: 0,
 	}
 
 	var position;
@@ -34,12 +42,14 @@ function initGeoloc(callback) {
 	// La géolocation est-elle prise en charger par le navigateur ?
 	if (navigator.geolocation)
   	{
-        navigator.geolocation.getCurrentPosition(position, errorHandler, {enableHighAccuracy:false, maximumAge:60000, timeout:27000});
-    	_geoData._Latitude = position.coords.latitude;
-    	_geoData._Longitude = position.coords.longitude;		
-    }
-    else
-        alert('Votre navigateur ne prend malheureusement pas en charge la géolocalisation.');
+        	navigator.geolocation.getCurrentPosition(position, 
+							errorHandler, 
+							{enableHighAccuracy:false, maximumAge:60000, timeout:27000});
+    		_geoData._Latitude = position.coords.latitude;
+    		_geoData._Longitude = position.coords.longitude;		
+  	}
+    	else
+       		 alert('Votre navigateur ne prend malheureusement pas en charge la géolocalisation.');
 }
 
 // Fonction de gestion des erreurs
